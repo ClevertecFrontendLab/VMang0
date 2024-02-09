8e8db8608d8db055a9b62f58273dcadc3397127c
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-10 01:46:43 +0300

    refactor: delete breakpoints for ant

--- src/components/main/app.jsx
+++ src/components/main/app.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { Layout } from 'antd';
 import Sidebar from '@components/sidebar/index.jsx';
 const { Header, Content } = Layout;
-import { ConfigProvider } from 'antd';
-import { customBreakpoints } from '../../assets/data/custom-breakpoints/index.js';
 
 const App = () => {
     return (
-        <ConfigProvider breakpoints={ customBreakpoints }>
-            <Layout className='app'>
-                <Sidebar />
-                <Layout>
-                    <Header>Header</Header>
-                    <Content className='app__content'>Content</Content>
-                </Layout>
+        <Layout className='app'>
+            <Sidebar />
+            <Layout>
+                <Header>Header</Header>
+                <Content className='app__content'>Content</Content>
             </Layout>
-        </ConfigProvider>
+        </Layout>
     );
 };
 

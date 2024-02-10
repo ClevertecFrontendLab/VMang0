0c3844e371b6f20f31aba2c368a3b2a40cb450f6
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-11 01:53:52 +0300

    feat(logo): add mobile version

--- src/components/sidebar/logo/logo.jsx
+++ src/components/sidebar/logo/logo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.png';
 import logo_collapsed from './logo_collapsed.png';
+import classNames from 'classnames';
 
-const Logo = ({ collapsed }) => {
+const Logo = ({ collapsed, isMobile }) => {
+    const logoClasses = classNames('logo', {
+        'logo__collapsed': collapsed && !isMobile,
+        'logo__mobile': isMobile,
+        'logo__mobile__collapsed': collapsed && isMobile,
+    });
+
     return (
-        <div className={`logo ${collapsed ? 'logo__collapsed' : ''}`}>
+        <div className={logoClasses}>
             <img src={collapsed ? logo_collapsed : logo} alt="cleverFIT logotype"/>
         </div>
     );

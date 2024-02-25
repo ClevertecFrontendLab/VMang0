dc16ecac6325662355a5d9f6f76de7215d5e37c4
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-25 04:21:26 +0300

    feat: add main store

--- src/redux/configure-store.ts
+++ src/redux/configure-store.ts
@@ -1,8 +1,24 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { createReduxHistoryContext } from 'redux-first-history';
+import { createBrowserHistory } from 'history';
+import userReducer from './slices/userSlice';
+
+const { routerMiddleware, createReduxHistory, routerReducer } =
+    createReduxHistoryContext({ history: createBrowserHistory(), savePreviousLocations: 1 });
+
+const rootReducer = combineReducers({
+    router: routerReducer,
+    user: userReducer,
+});
 
 export const store = configureStore({
-    reducer: {},
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(routerMiddleware),
 });
+
+export const history = createReduxHistory(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

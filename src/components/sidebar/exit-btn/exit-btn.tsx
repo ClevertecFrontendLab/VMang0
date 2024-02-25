b517bd98b5264cdd1a89184326b1b4628bdf6834
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-25 04:21:43 +0300

    feat: add logic for logout

--- src/components/sidebar/exit-btn/exit-btn.tsx
+++ src/components/sidebar/exit-btn/exit-btn.tsx
@@ -2,6 +2,8 @@ import {FC} from 'react';
 import { ExitIcon } from '@utils/icons/exit-icon';
 import { Button } from 'antd';
 import classNames from 'classnames';
+import { useDispatch } from 'react-redux';
+import { userLogout } from '@redux/slices/userSlice';
 
 type Props = {
     collapsed: boolean,
@@ -9,14 +11,22 @@ type Props = {
 }
 
 export const ExitBtn: FC<Props> = ({ collapsed, isMobile }) => {
+    const dispatch = useDispatch();
     const exitBtnClasses = classNames('exit_btn', {
         'exit_btn__collapsed': collapsed && !isMobile,
         'exit_btn__mobile': isMobile,
         'exit_btn__mobile__collapsed': collapsed && isMobile,
     });
+    const handleLogout = () => {
+        dispatch(userLogout());
+    }
     return (
         <div className={exitBtnClasses}>
-            <Button icon={<ExitIcon />} aria-label='exit'>Выход</Button>
+            <Button icon={<ExitIcon />}
+                    aria-label='exit'
+                    onClick={handleLogout}>
+                Выход
+            </Button>
         </div>
     );
 };

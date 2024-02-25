d168a6b1a4914eaaceeab1a3521eb234ee05cf9e
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-25 04:23:27 +0300

    feat: add result path

--- src/utils/constants/route-path/route-path.ts
+++ src/utils/constants/route-path/route-path.ts
@@ -1,3 +1,8 @@
 export const MAIN = '/main';
 export const LOGIN = '/auth';
 export const REGISTRATION = '/auth/registration';
+
+export const ERROR_LOGIN = '/result/error-login';
+export const SUCCESS_REGISTRATION = '/result/success';
+export const ERROR_REGISTRATION = '/result/error';
+export const ERROR_REG_USER_EXIST = '/result/error-user-exist';

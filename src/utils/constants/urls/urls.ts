c633632b0dbc6636a309ee6c57e8c9acd2154683
VMang0 <valeriakorolkova2703@gmail.com>
2024-02-27 18:49:03 +0300

    feat: new urls and routes for recovery password

--- src/utils/constants/urls/urls.ts
+++ src/utils/constants/urls/urls.ts
@@ -1,3 +1,6 @@
 export const BASE_URL = 'https://marathon-api.clevertec.ru';
 export const LOGIN_URL = '/auth/login';
 export const REGISTRATION_URL = '/auth/registration';
+export const CHECK_EMAIL_URL = '/auth/check-email';
+export const CONFIRM_EMAIL_URL = '/auth/confirm-email';
+export const CHANGE_PASSWORD_URL = '/auth/change-password';

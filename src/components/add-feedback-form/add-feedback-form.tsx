b8c39fb13c0170460560f63a6292085285a3fccc
VMang0 <valeriakorolkova2703@gmail.com>
2024-03-06 10:20:47 +0300

    feat: form to add new feedback

---
+++ src/components/add-feedback-form/add-feedback-form.tsx
@@ -0,0 +1,66 @@
+import { FC, useEffect, useState } from 'react';
+import { Modal } from 'antd';
+import { CustomRate, PrimaryButton } from '@utils/style/ant/custom';
+import { StarFilled, StarOutlined } from '@ant-design/icons';
+import TextArea from 'antd/es/input/TextArea';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '@redux/configure-store';
+import { addFeedback } from '@redux/actions/feedback';
+import './add-feedback-form.scss';
+
+type Props = {
+    isModalOpen: boolean,
+    handleModal: () => void
+}
+
+export const AddFeedbackForm: FC<Props> = ({ isModalOpen, handleModal }) => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [message, setMessage] = useState<string>('');
+    const [rating, setRating] = useState<number>(0);
+    const isAddSuccess = useSelector((state) => state.feedback.isAddSuccess);
+
+    useEffect(() => {
+        if (isAddSuccess) {
+            setRating(0);
+            setMessage('');
+        }
+    }, [isAddSuccess])
+
+    const handlePublish = async () => {
+        await dispatch(addFeedback({ message, rating }));
+        handleModal();
+    }
+
+    return (
+        <Modal
+            title='Ваш отзыв'
+            centered
+            open={isModalOpen}
+            width={539}
+            className='add_feedback_modal'
+            onCancel={handleModal}
+            zIndex={100}
+            footer={
+                <PrimaryButton onClick={handlePublish}
+                               width={{ small: '100%' }}
+                               disabled={rating < 1}
+                               data-test-id='new-review-submit-button'>
+                    Опубликовать
+                </PrimaryButton>
+            }>
+            <CustomRate defaultValue={4}
+                        size={20}
+                        height={'24px'}
+                        value={rating}
+                        onChange={(e) => setRating(e)}
+                        character={({ value, index }) => {
+                            return value && index! < value ?
+                                <StarFilled /> : <StarOutlined style={{ fontSize: '23px' }}/>
+                        }}  />
+            <TextArea placeholder='Расскажите, почему Вам понравилось наше приложение'
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      className='text_area'/>
+        </Modal>
+    );
+};
---
+++ src/components/add-feedback-form/index.ts
@@ -0,0 +1,1 @@
+export { AddFeedbackForm } from './add-feedback-form';

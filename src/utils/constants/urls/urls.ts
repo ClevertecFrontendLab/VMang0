3cc5565fc7389af9700cb84b4bff064a160d9ef6
VMang0 <valeriakorolkova2703@gmail.com>
2024-03-06 10:44:47 +0300

    feat(sprint_3): new routes and urls

--- src/utils/constants/urls/urls.ts
+++ src/utils/constants/urls/urls.ts
@@ -4,3 +4,4 @@ export const REGISTRATION_URL = '/auth/registration';
 export const CHECK_EMAIL_URL = '/auth/check-email';
 export const CONFIRM_EMAIL_URL = '/auth/confirm-email';
 export const CHANGE_PASSWORD_URL = '/auth/change-password';
+export const FEEDBACK_URL = '/feedback';
